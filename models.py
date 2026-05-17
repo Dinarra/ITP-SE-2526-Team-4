4432afd1730bc42ee4441779835adfbdc1005533
Dinara Nurzhaubay <254303@astanait.edu.kz>
2026-05-17 21:20:54 +0500

    add string representation and engine class

--- models.py
+++ models.py
@@ -7,3 +7,10 @@ class User:
     def init(self, user_id, liked_items):
         self.user_id = user_id
         self.liked_items = set(liked_items)
+
+    def str(self):
+        return f"User {self.user_id} | Items: {len(self.liked_items)}"
+
+    class RecommendationEngine:
+        def init(self, users):
+            self.users = users
