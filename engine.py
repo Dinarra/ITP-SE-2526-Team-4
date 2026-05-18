1c71841d0a3273f47c5cc2b1c451ca4e988a9ade
Balzhan <balzanamantaeva7@gmail.com>
2026-05-18 20:47:36 +0500

    feat:develop user similarity tracking via set intersections

--- engine.py
+++ engine.py
@@ -1,16 +1,7 @@
-def get_similar_users(target_user, all_users):
+def get_similar_users(target_user, all_users_dict):
     similar = []
-    for user in all_users:
-        if user.user_id != target_user.user_id:
-            if target_user.liked_items.intersection(user.liked_items):
+    for u_id, user in all_users_dict.items():
+        if u_id != target_user.user_id:
+            if target_user.watched_history.intersection(user.watched_history):
                 similar.append(user)
     return similar
-
-def generate_recommendations(target_user, similar_users):
-    recommended = set()
-    for other in similar_users:
-        new_items = other.liked_items - target_user.liked_items
-        for item in new_items:
-            if item not in recommended:
-                recommended.add(item)
-                yield item
